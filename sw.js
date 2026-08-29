35853615e58212b78eb5aeeec06ecd5bb84f12ef
gingerbeard-drifts <mulvaneysp@gmail.com>
2026-08-29 11:26:55 -0400

    Cache split GBW production assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gbw-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./gbw-mark.svg","./gbw-widget.js"];
+const CACHE="gbw-v1.0.1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./gbw-mark.svg","./gbw-widget.js","./app.css","./app-core.js","./app-chart.js","./app-ui.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>{
