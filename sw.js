5a60ca002de37d2e66a10848542f56d677879be5
gingerbeard-drifts <mulvaneysp@gmail.com>
2026-08-29 12:27:46 -0400

    Cache GBW mobile chart polish asset

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gbw-v1.0.2";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./gbw-mark.svg","./gbw-widget.js","./app.css","./app-core.js","./app-chart.js","./app-ui.js"];
+const CACHE="gbw-v1.0.3";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./gbw-mark.svg","./gbw-widget.js","./app.css","./app-core.js","./app-chart.js","./app-polish.js","./app-ui.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
